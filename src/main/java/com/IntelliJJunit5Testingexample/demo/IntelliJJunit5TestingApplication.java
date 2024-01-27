75ff859409cd016362314fe6a5c035f39f5d9f5d
Fatima <fatimahnoor05@gmail.com>
2024-01-27 22:09:17 +0200

    backend structure implemented

---
+++ src/main/java/com/IntelliJJunit5Testingexample/demo/IntelliJJunit5TestingApplication.java
@@ -0,0 +1,13 @@
+package com.IntelliJJunit5Testingexample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJJunit5TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliJJunit5TestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IntelliJJunit5Testingexample/demo/IntelliJJunit5TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntelliJJunit5Testingexample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliJJunit5TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
